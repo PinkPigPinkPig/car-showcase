076145468ef04db06e001896733f5639ea7ebb11
PinkPigPinkPig <vuquytuan2706@gmail.com>
2023-12-25 23:23:58 +0700

    create client project

---
+++ car-showcase-client/app/page.tsx
@@ -0,0 +1,28 @@
+import { CustomFilter, Hero, SearchBar } from '@/components'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+
+      <div className='mt-12 padding-x max-width' id='discover'>
+        <div className='home_text-container'>
+          <h1 className='text-4xl font font-extrabold'>
+            Car Catalogue
+          </h1>
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home_filter-container'>
+            {/* <CustomFilter title="fuel" />
+            <CustomFilter title="year" /> */}
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
